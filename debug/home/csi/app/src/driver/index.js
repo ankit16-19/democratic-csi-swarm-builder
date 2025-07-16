102331e9d53899b639e83425e018fb2b6ab001ee
Marius Guščius <marius.guscius@mailbox.org>
2025-07-16 16:02:37 +0300

    to string

--- debug/home/csi/app/src/driver/index.js
+++ debug/home/csi/app/src/driver/index.js
@@ -939,7 +939,7 @@ class CsiBaseDriver {
               driver.ctx.logger.info("LOOP <<<<<<<<");
               while (!result) {
                 await GeneralUtils.sleep(2000);
-                driver.ctx.logger.info("LOOP A <<<<<<", device);
+                driver.ctx.logger.info("LOOP A <<<<<<", device.toString());
                 result = await filesystem.pathExists(device);
                 driver.ctx.logger.info("LOOP B <<<<<<", result);
                 if (result) {
